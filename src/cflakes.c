cf412676a6f603b7694da147e365fb54d8dce84a
Hope Ranker <hope.ranker@gmail.com>
2018-09-27 23:27:33 -0400

    randomize all the parameters

--- src/cflakes.c
+++ src/cflakes.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "cell6.h"
 #include "path.h"
@@ -7,21 +8,38 @@
 #define MAX_DIM 200
 #define MAX_ITERS 10000
 
-#define BG_LEVEL 0.4
-#define CONST_GAIN 0.001
-#define NDIFF_WEIGHT 1.0
-#define SVG_DIM "8in"
+#define BG_BASE 0.3
+#define BG_VAR 0.5
+#define GAIN_BASE 0.0
+#define GAIN_VAR 0.005
+#define NDW_BASE 0.5
+#define NDW_VAR 2.5
+#define DIM_BASE 2.0
+#define DIM_VAR 1.0
+
+static float rand_float(float base, float var) {
+  return base + (float) rand() * var / (float) RAND_MAX;
+}
 
 int main(int argc, char **argv) {
   struct c6_state_geometry *geo = c6_make_geometry(MAX_DIM);
   struct c6_state *state = c6_make_state(geo);
 
-  rsf_init_state(state, BG_LEVEL);
+  float bg_level = rand_float(BG_BASE, BG_VAR);
+  float const_gain = rand_float(GAIN_BASE, GAIN_VAR);
+  float ndiff_weight = rand_float(NDW_BASE, NDW_VAR);
+  float svg_dim = rand_float(DIM_BASE, DIM_VAR);
+
+  char svg_dim_s[12] = {};;
+  snprintf(svg_dim_s, sizeof(svg_dim_s)-1, "%8.6fin", svg_dim);
+
+
+  rsf_init_state(state, bg_level);
   state = rsf_state_advance_to_edge(
-      state, MAX_ITERS, CONST_GAIN, NDIFF_WEIGHT);
+      state, MAX_ITERS, const_gain, ndiff_weight);
 
   struct pa_path_set *ps = rsf_make_traced_path_set(state);
-  svg_print_paths(ps, MAX_DIM, SVG_DIM);
+  svg_print_paths(ps, MAX_DIM, svg_dim_s);
 
   pa_free_path_set(ps);
   c6_free_state(state);
